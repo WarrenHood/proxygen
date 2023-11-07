335a3affe00589be96581c12646248c6a0e07a33
Warren Hood <nullbyte001@gmail.com>
2023-11-07 19:37:51 +0200

    Print out DLL arch when getting exports

--- src/exports.rs
+++ src/exports.rs
@@ -1,9 +1,11 @@
 use anyhow::Result;
-use exe::{ExportDirectory, VecPE};
+use exe::{ExportDirectory, VecPE, PE};
 use std::path::PathBuf;
 
 pub fn get_exports(pe_file: &PathBuf) -> Result<Vec<String>> {
+    println!("Getting exports for {}", pe_file.to_string_lossy());
     let pe = VecPE::from_disk_file(pe_file)?;
+    println!("Detected arch: {:?}", pe.get_arch()?);
     let export_directory = ExportDirectory::parse(&pe)?;
     let mut results: Vec<String> = export_directory
         .get_export_map(&pe)?
