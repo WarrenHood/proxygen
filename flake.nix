fed08bdfcf72a7b8c95a922a519e8b3f3d9d137c
Warren Hood <nullbyte001@gmail.com>
2024-11-13 02:14:00 +0200

    Chore: Update flake.nix to build the proxygen package by default

--- flake.nix
+++ flake.nix
@@ -12,7 +12,12 @@
         naersk-lib = pkgs.callPackage naersk { };
       in
       {
-        defaultPackage = naersk-lib.buildPackage ./.;
+        defaultPackage = naersk-lib.buildPackage {
+          pname = "proxygen";
+          src = ./.;
+          cargoBuildOptions = x: x ++ [ "-p" "proxygen" ];
+          cargoTestOptions = x: x ++ [ "-p" "proxygen" ];
+        };
         devShell = with pkgs; mkShell {
           buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy rust-analyzer git-cliff ];
           RUST_SRC_PATH = rustPlatform.rustLibSrc;
